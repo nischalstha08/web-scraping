8258cce8b923f065c625c950ecaca73975a7fe18
Nischal Shrestha <nischalstha08@gmail.com>
2025-07-01 08:19:50 +0545

    Updated settings and booksdata.csv

--- part 6/bookscraper/bookscraper/settings.py
+++ part 6/bookscraper/bookscraper/settings.py
@@ -68,9 +68,10 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-ITEM_PIPELINES = {
-   "bookscraper.pipelines.BookscraperPipeline": 300,
-}
+#ITEM_PIPELINES = {
+#   "bookscraper.pipelines.BookscraperPipeline": 300,
+#   "bookscraper.pipelines.SaveToMySQLPipeline": 400,
+#}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
